3ff94144b3561bf3a62641f5c3f1c9820d9a806d
成瀬恵介 <k-naruse@ntk.mygbiz.com>
2018-02-27 18:29:57 +0900

    Initial Commit

---
+++ Meditation Timer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Meditation Timer
+//
+//  Created by プリュスコン on 2018/02/27.
+//  Copyright © 2018年 naruzo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
